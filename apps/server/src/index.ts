1c0bdcad264a34a1394da2ac822f9c630dd8e23a
arjun31 <arjunbanerjee13@gmail.com>
2025-05-22 01:30:36 +0530

    refactor: set sameSite to none

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -57,8 +57,8 @@ app.use(
         cookie: {
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7days
             secure: process.env.NODE_ENV ? false : true,
+            sameSite: "none",
             httpOnly: true, // server only
-            sameSite: "strict",
         },
     }),
 );
