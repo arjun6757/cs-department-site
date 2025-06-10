6fdd7db270dcfc804d63a8a5eb8c34066007777c
arjun31 <arjunbanerjee13@gmail.com>
2025-06-10 12:59:14 +0530

    feat: redesign landing page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Carousel from "./carousel.jsx";
 import {
 	ChevronRight,
 	Menu,
@@ -7,29 +6,32 @@ import {
 	UploadCloud,
 	UserCheck,
 } from "lucide-react";
-import * as Icons from "./svg/index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Index() {
 	const [isHidden, setIsHidden] = useState(true);
+	const [demo, setDemo] = useState("/images/demo-desktop.png");
+
 	const touch = "ontouchstart" in window || navigator.maxTouchPoints > 0;
 
-	const images = [
-		{
-			src: "/images/slide-1.jpg",
-			alt: "Bankura Sammilani College Entrance",
-		},
-		{ src: "images/slide-2.jpg", alt: "Acharaya Prafulla Chandra Block" },
-		{
-			src: "images/slide-3.jpg",
-			alt: "View from inside of Bankura Sammilani College",
-		},
-	];
+	useEffect(() => {
+		handleResize();
+		window.addEventListener("resize", handleResize);
+		return () => removeEventListener("resize", handleResize);
+	}, []);
+
+	const handleResize = () => {
+		if (window.innerWidth >= 640) {
+			setDemo("/images/demo-desktop.png");
+		} else {
+			setDemo("/images/demo-mobile.jpeg");
+		}
+	};
 
 	return (
-		<div className="min-h-screen text-sm text-gray-700 bg-white">
-			<header>
-				<nav className="p-4 flex flex-col gap-2 bg-[#212121] text-gray-100">
+		<div className="min-h-screen w-full text-sm bg-[#171717] text-gray-300">
+			<header className="sticky top-0">
+				<nav className="p-4 flex flex-col gap-2 bg-[#171717]/50 backdrop-blur-md text-gray-300 border-b border-[#333] transition-colors">
 					<button
 						data-hidden={isHidden}
 						onClick={() => setIsHidden((p) => !p)}
@@ -70,7 +72,7 @@ export default function Index() {
 									<span>Student</span>
 									<ChevronRight className="w-4 h-4 sm:rotate-90 sm:group-open:-rotate-90 group-open:rotate-90 transition-transform duration-300" />
 								</summary>
-								<ul className="lg:absolute lg:left-0 lg:bg-white lg:text-gray-800 p-2 pl-6 lg:p-4 lg:rounded-md lg:shadow-md lg:z-10 flex flex-col gap-2">
+								<ul className="lg:absolute lg:left-0 lg:bg-white lg:text-gray-700 p-2 pl-6 lg:p-4 lg:rounded-md lg:shadow-md lg:z-10 flex flex-col gap-2">
 									<li className="lg:hover:text-blue-500">
 										<Link to="/login">Login</Link>
 									</li>
@@ -133,39 +135,58 @@ export default function Index() {
 						</div>
 					</ul>
 				</nav>
-
-				<Carousel images={images} />
 			</header>
 
 			<main className="my-6">
 				<div
 					id="home"
-					className="my-[5rem] w-xs md:w-lg lg:w-4xl mx-auto flex flex-col gap-2 sm:gap-5 p-8"
+					className="mt-[2rem] sm:mt-[5rem] _mobile-width sm:w-xl md:w-2xl lg:w-4xl mx-auto flex flex-col gap-5 sm:gap-6 p-2 sm:p-8 text-sm lg:text-[16px]"
 				>
-					<h1 className="text-xl sm:text-2xl font-bold text-center ">
-						Introduction
+					<h1 className="_mobile-text text-lg sm:text-xl md:text-2xl lg:text-4xl font-bold text-center">
+						Department of Computer Science
 					</h1>
-					<p className="text-gray-500 text-sm">
-						A simple solution for students and faculty to access
-						academic resources, manage attendance, and securely
-						upload or download past question papers — built with the
-						MERN stack.
+
+					<p className="_mobile-description text-gray-400 text-sm md:text-[16px]">
+						A centralized platform for students and faculty to track
+						attendance, access academic resources, and manage past
+						question papers — all in one place.
 					</p>
+
+					<div className="flex gap-4 justify-center items-center mt-3 sm:mt-2">
+						<Link
+							to="/dashboard"
+							className="px-4 py-2 bg-blue-500 hover:opacity-90 active:opacity-80 rounded text-white"
+						>
+							Get Started
+						</Link>
+						<Link
+							to="/signup"
+							className="px-4 py-2 bg-white hover:opacity-90 active:opacity-80 text-black rounded flex gap-2 items-center"
+						>
+							Signup
+						</Link>
+					</div>
+
+					<div className="border border-[#333] rounded-lg overflow-hidden mt-4 sm:mt-2">
+						<img
+							loading="lazy"
+							src={demo}
+							alt="cs-department-site demo image"
+						/>
+					</div>
 				</div>
 
 				<div
 					id="features"
-					className="mt-[5rem] flex flex-col gap-1 items-center px-4 py-12 bg-gray-100"
+					className="p-2 sm:p-8 md:px-4 py-6 sm:py-12 sm:px-0 _mobile-width sm:w-fit mx-auto "
 				>
-					<h2 className="text-lg sm:text-xl font-bold">Features</h2>
-
-					<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-15 sm:gap-8 xl:gap-20 mt-8 xl:p-4">
-						<li className="space-y-2.5 sm:space-y-4 p-5 w-[18rem] xl:w-[20rem] h-[15rem] rounded-lg bg-white shadow-custom">
+					<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-8 xl:gap-20 xl:p-4">
+						<li className="border border-[#333] hover:border-gray-500 space-y-2.5 sm:space-y-4 p-5 sm:w-lg md:w-[18rem] xl:w-[20rem] md:h-[15rem] rounded-lg">
 							<h3 className="text-[1em] xl:text-lg font-medium inline-flex gap-2 items-center">
 								<UserCheck className="text-blue-500" />
 								Online Attendance System
 							</h3>
-							<p className="text-gray-600 text-sm xl:text-[1em]">
+							<p className="text-gray-400 text-sm xl:text-[1em]">
 								A planned system to manage and track student
 								attendance online. Will include a role-based
 								login for students and faculty, allowing faculty
@@ -174,12 +195,12 @@ export default function Index() {
 								both the client and server side.
 							</p>
 						</li>
-						<li className="space-y-2.5 sm:space-y-4 p-5 w-[18rem] xl:w-[20rem] h-[15rem] rounded-lg bg-white shadow-custom">
+						<li className="border border-[#333] hover:border-gray-500 space-y-2.5 sm:space-y-4 p-5 sm:w-lg md:w-[18rem] xl:w-[20rem] md:h-[15rem] rounded-lg">
 							<h3 className="text-[1em] xl:text-lg font-medium inline-flex gap-2 items-center">
 								<UploadCloud className="text-blue-500" />
 								Easily Upload & Get PYQs
 							</h3>
-							<p className="text-gray-600 text-sm xl:text-[1em]">
+							<p className="text-gray-400 text-sm xl:text-[1em]">
 								Users can upload and download previous year
 								question papers (PYQs). Uploaded entries are
 								stored in a database along with optional note
@@ -188,12 +209,12 @@ export default function Index() {
 								and notes if available.
 							</p>
 						</li>
-						<li className="space-y-2.5 sm:space-y-4 p-5 w-[18rem] xl:w-[20rem] h-[15rem] rounded-lg bg-white shadow-custom">
+						<li className="border border-[#333] hover:border-gray-500 space-y-2.5 sm:space-y-4 p-5 sm:w-lg md:w-[18rem] xl:w-[20rem] md:h-[15rem] rounded-lg">
 							<h3 className="text-[1em] xl:text-lg font-medium inline-flex gap-2 items-center">
 								<Shield className="text-blue-500" />
 								Role-Based User Access
 							</h3>
-							<p className="text-gray-600 text-sm xl:text-[1em]">
+							<p className="text-gray-400 text-sm xl:text-[1em]">
 								Role verification is handled on both client and
 								server sides to control access to different
 								sections of the site. For example, only admins
@@ -204,35 +225,15 @@ export default function Index() {
 						</li>
 					</ul>
 				</div>
-
-				<div
-					id="tech-stack"
-					className="flex flex-col gap-8 items-center py-12"
-				>
-					<h2 className="text-lg sm:text-xl font-bold ">
-						Tech Stack
-					</h2>
-
-					<div className="grid grid-cols-5 md:grid-cols-6 lg:grid-cols-10 gap-5 md:gap-6 xl:gap-10">
-						{Icons.svgIcons.map(
-							({ name, component: Component }) => (
-								<Component
-									key={name}
-									className="w-10 h-10 sm:w-12 sm:h-12 border border-[#ddd] p-2 rounded-lg shadow-xs select-none"
-								/>
-							),
-						)}
-					</div>
-				</div>
 			</main>
 
-			<footer className="flex flex-col gap-0 bg-[#171717] text-gray-300 px-12 py-6">
+			<footer className="flex flex-col gap-0 px-12 py-6 border-t border-[#333]">
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 sm:gap-20 my-6 ">
 					<div className="flex flex-col gap-4">
 						<h3 className="font-bold text-lg sm:text-xl">
 							CS Department Site
 						</h3>
-						<p className="text-sm">
+						<p className="text-sm text-gray-400">
 							A simple fullstack app to mimic a college site —
 							built with the MERN stack.
 						</p>
@@ -242,7 +243,7 @@ export default function Index() {
 						<h3 className="font-bold text-lg sm:text-xl">
 							Quick Links
 						</h3>
-						<ul className="flex gap-8">
+						<ul className="flex gap-8 text-gray-400">
 							<div className="flex flex-col gap-2">
 								<li>
 									<Link to="/login">User Login</Link>
@@ -272,7 +273,7 @@ export default function Index() {
 						<h3 className="font-bold text-lg sm:text-xl">
 							Contact Us
 						</h3>
-						<div>
+						<div className="text-gray-400 flex flex-col gap-2">
 							<p>
 								Bankura Sammilani College, Kenduadihi, Bankura,
 								West Bengal - 722102
