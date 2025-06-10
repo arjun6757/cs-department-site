09f96640ad73a79860ccbad2c9edc634f71ffe2f
arjun31 <arjunbanerjee13@gmail.com>
2025-06-10 13:08:07 +0530

    chore: cleanup legacy assets no longer in use

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -5,35 +5,3 @@ export function formattedDate(date) {
     const day = date.getDate().toString().padStart(2, "0");
     return `${year}-${month}-${day}`;
 }
-
-export function getPrevDate(date) {
-    if (!(date instanceof Date)) return;
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    return new Date(year, month, day - 1)
-}
-
-export function getNextDate(date) {
-    if (!(date instanceof Date)) return;
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    return new Date(year, month, day + 1)
-}
-
-export function getLastWeekDate(date) {
-    if (!(date instanceof Date)) return;
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    return new Date(year, month, day - 7);
-}
-
-export function getLastMonthDate(date) {
-    if (!(date instanceof Date)) return;
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    return new Date(year, month - 1, day);
-}
