db5b7d57e5c70dab4068884ae394e7408694e682
arjun31 <arjunbanerjee13@gmail.com>
2025-06-15 21:14:20 +0530

    fix: entry router

--- server/src/router/entry.router.ts
+++ server/src/router/entry.router.ts
@@ -2,15 +2,16 @@ import express from "express";
 import { upload } from "../middleware/multer.middleware";
 import { getAllEntries, handleEntryDelete, handleUpload } from "../controllers/entry.controller";
 import isAdmin from "../middleware/isadmin.middleware";
+import isAuthenticated from "../middleware/isauthenticated.middleware";
 
 const router = express.Router();
 
 // @ts-ignore
 // as Request != ModRequest
 
-router.post("/upload", isAdmin, upload.single("document"), handleUpload);
+router.post("/upload", isAuthenticated, isAdmin, upload.single("document"), handleUpload);
 
-router.get("/all", getAllEntries);
-router.post("/delete/:id", isAdmin, handleEntryDelete);
+router.get("/all", isAuthenticated, getAllEntries);
+router.post("/delete/:id", isAuthenticated, isAdmin, handleEntryDelete);
 
 export default router;
